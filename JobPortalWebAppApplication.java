f453840858d0117abc98352027bd3d9bcff8d39b
codewithhermes <blue.lock2313@gmail.com>
2024-09-30 18:23:53 +0530

    Add files via upload

---
+++ JobPortalWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.ashu.JobPortalWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobPortalWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobPortalWebAppApplication.class, args);
+	}
+
+}
