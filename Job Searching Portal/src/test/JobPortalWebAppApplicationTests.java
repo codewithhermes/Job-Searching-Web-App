a78fb805610f09761c56fbeccb69616ef31b8090
codewithhermes <blue.lock2313@gmail.com>
2024-09-30 18:45:36 +0530

    Add files via upload

---
+++ Job Searching Portal/src/test/JobPortalWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashu.JobPortalWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobPortalWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
